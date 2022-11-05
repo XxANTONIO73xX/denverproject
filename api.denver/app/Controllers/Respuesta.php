95f4f1c081313d1aa3af83a83217b650086e0aad
XxANTONIO73xX <60475171+XxANTONIO73xX@users.noreply.github.com>
2022-11-04 21:46:38 -0700

    Historia de usuario #6 - Creación del módulo respuesta

---
+++ api.denver/app/Controllers/Respuesta.php
@@ -0,0 +1,71 @@
+<?php
+namespace App\Controllers;
+use CodeIgniter\RESTful\ResourceController;
+use App\Models\RespuestaModel;
+use CodeIgniter\HTTP\Response;
+
+class Respuesta extends ResourceController{
+    protected $modelName = 'App\Models\RespuestaModel';
+    protected $format = 'json';
+
+    public function index()
+    {
+        $data=[
+            "respuestas" => $this->model->findAll()
+        ];
+        return $this->respond($data);
+    }
+
+    public function show($id=NULL){
+        $data=["respuesta" => $this->model->find($id)];
+        return $this->respond($data);
+    }
+
+    public function create()
+    {
+        $data=[
+            "idPadre" => $this->request->getPost("idPadre"),
+            "idActividad" => $this->request->getPost("idActividad"),
+            "idTopico" => $this->request->getPost("idTopico"),
+            "respuestaUsuario" => $this->request->getPost("respuestaUsuario"),
+        ];
+
+        $id = $this->model->insert($data);
+
+        if($id){
+            return $this->respond($this->model->find($id));
+        }else{
+            return $this->respond(["error" => "Hubo un error al insertar"]);
+        }
+    }
+
+    public function update($id = null){
+        $data = [];
+        if(!empty($this->request->getPost("idPadre")))
+            $data["idPadre"] = $this->request->getPost("idPadre");
+        if(!empty($this->request->getPost("idActividad")))
+            $data["idActividad"] = $this->request->getPost("idActividad");
+        if(!empty($this->request->getPost("idTopico")))
+            $data["idTopico"] = $this->request->getPost("idTopico");
+        if(!empty($this->request->getPost("respuestaUsuario")))
+            $data["respuestaUsuario"] = $this->request->getPost("respuestaUsuario");
+        
+        $result = $this->model->update($id, $data);
+
+        if($result){
+            return $this->respond(["result" => "El registro se edito correctamente", "terapeuta" => $this->model->find($id)]);
+        }else{
+            return $this->respond(["error" => "hubo un error al editar"]);
+        }
+    }
+
+    public function delete($id = null){
+        $result = $this->model->delete($id);
+        if($result){
+            return $this->respond(["result"=> "El registro se elimino correctamente"]);
+        }else{
+            return $this->respond((["error" => "hubo un error al eliminar"]));
+        }
+    }
+
+}
---
+++ api.denver/app/Models/RespuestaModel.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Models;
+use CodeIgniter\Model;
+
+class RespuestaModel extends Model{
+    protected $table = "respuestas";
+    protected $primaryKey = "id";
+    protected $allowedFields = [
+        "id",
+        "idPadre",
+        "idActividad",
+        "idTopico",
+        "respuestaUsuario",
+    ];
+}
