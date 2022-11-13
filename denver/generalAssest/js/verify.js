9ff3de957977eff480791a8107a8a9bcacde2c01
XxANTONIO73xX <60475171+XxANTONIO73xX@users.noreply.github.com>
2022-11-13 15:51:58 -0700

    reset

--- denver/generalAssest/js/verify.js
+++
@@ -1,8 +0,0 @@
-if(sessionStorage.getItem('usuario') === null){
-    location.href = '/index.html';
-}
-
-$("#cerrarSesion").click(function (e) { 
-    sessionStorage.removeItem('usuario')
-    location.href = '/index.html';
-});
