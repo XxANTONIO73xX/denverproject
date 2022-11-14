cc1dc4933367ac5c888deff6b5579c17e00a280c
pablx33 <ojedaaguirrep@gmail.com>
2022-11-13 23:13:23 -0700

    cambio equis

--- public/assets/Curriculum/js/botones.js
+++ public/assets/Curriculum/js/botones.js
@@ -1,50 +1,3 @@
-function showContent() {
-    element = document.getElementById("createTopico");
-    selectTopico = document.getElementById("selectTopico");
-    check = document.getElementById("check");
-    if (check.checked) {
-        element.style.display = 'block';
-        selectTopico.style.display = 'none';
-    }
-    else {
-        element.style.display = 'none';
-        selectTopico.style.display = 'block';
-    }
-}
-
-function abrirModalEditar() {
-    var modalBg = document.querySelector('.modal-bg');
-    modalBg.classList.add('bg-active');
-}
-
-function cerrarModalEditar() {
-    var modalBg = document.querySelector('.modal-bg');
-    modalBg.classList.remove('bg-active');
-}
-
-function abrirModalVisualizar() {
-    var modalBg = document.querySelector('.modal-visualizar_bg');
-    modalBg.classList.add('bg-active-vis');
-}
-
-function cerrarModalVisualizar() {
-    var modalBg = document.querySelector('.modal-visualizar_bg');
-    modalBg.classList.remove('bg-active-vis');
-}
-
-var modalBtn = document.querySelector('.boton-agregar_modal');
-var modalBg = document.querySelector('.modal-bg');
-var modalClose = document.querySelector('.btn-cancelar');
-
-modalBtn.addEventListener('click', function () {
-    modalBg.classList.add('bg-active');
-});
-
-modalClose.addEventListener('click', function () {
-    modalBg.classList.remove('bg-active');
-});
-
-
 function quedoAgregado() {
 
     var modalBg = document.querySelector('.modal-bg');
@@ -76,5 +29,4 @@ function quedoAgregado() {
         .fail(function () {
             console.log("Error", "Ocurrio un problema al guardar la cita")
         })
-
 }
--- public/assets/Curriculum/js/dataTable.js
+++ public/assets/Curriculum/js/dataTable.js
@@ -1,3 +1,49 @@
+function showContent() {
+    element = document.getElementById("createTopico");
+    selectTopico = document.getElementById("selectTopico");
+    check = document.getElementById("check");
+    if (check.checked) {
+        element.style.display = 'block';
+        selectTopico.style.display = 'none';
+    }
+    else {
+        element.style.display = 'none';
+        selectTopico.style.display = 'block';
+    }
+}
+
+function abrirModalEditar() {
+    var modalBg = document.querySelector('.modal-bg');
+    modalBg.classList.add('bg-active');
+}
+
+function cerrarModalEditar() {
+    var modalBg = document.querySelector('.modal-bg');
+    modalBg.classList.remove('bg-active');
+}
+
+function abrirModalVisualizar() {
+    var modalBg = document.querySelector('.modal-visualizar_bg');
+    modalBg.classList.add('bg-active-vis');
+}
+
+function cerrarModalVisualizar() {
+    var modalBg = document.querySelector('.modal-visualizar_bg');
+    modalBg.classList.remove('bg-active-vis');
+}
+
+var modalBtn = document.querySelector('.boton-agregar_modal');
+var modalBg = document.querySelector('.modal-bg');
+var modalClose = document.querySelector('.btn-cancelar');
+
+modalBtn.addEventListener('click', function () {
+    modalBg.classList.add('bg-active');
+});
+
+modalClose.addEventListener('click', function () {
+    modalBg.classList.remove('bg-active');
+});
+
 $(document).ready( function () {
     var table = $('#table').DataTable({
         ajax: {
