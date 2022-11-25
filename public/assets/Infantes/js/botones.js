1bac10688aa54abcc36e6cea850e3f758d4f727d
josejc456 <60475088+josejc456@users.noreply.github.com>
2022-11-24 19:41:22 -0700

    cambio chido

--- public/assets/Infantes/js/botones.js
+++ public/assets/Infantes/js/botones.js
@@ -2,45 +2,42 @@ function agregar(id) {
     var modalBg = document.querySelector('.modal-bg');
     modalBg.classList.remove('bg-active');
     check = document.getElementById("check");
-    if(id === 0){
-        if(check.checked){
-            //patente pendiente malvados y asociados
-        }else{
-            $.ajax({
-                url: 'https://denvermx.online/public/infante',
-                type: 'POST',
-                data: {
-                    "nombre": $("#nombre").val(),
-                    "apellidos": $("#apellidos").val(),
-                    "edad": $("#edad").val(),
-                    "curp": $("#curp").val(),
-                    "diagnostico": $("#diagnostico").val()
-                },
-                dataType: "json",
+    if (id === 0) {
+        $.ajax({
+            url: 'https://denvermx.online/public/infante',
+            type: 'POST',
+            data: {
+                "nombre": $("#nombre").val(),
+                "apellidos": $("#apellidos").val(),
+                "edad": $("#edad").val(),
+                "curp": $("#curp").val(),
+                "diagnostico": $("#diagnostico").val()
+            },
+            dataType: "json",
+        })
+            .done(function (data, res) {
+                if (data.hasOwnProperty('error')) {
+                    Swal.fire({
+                        position: 'center',
+                        icon: 'error',
+                        title: 'Ha ocurrido un error',
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                } else {
+                    Swal.fire({
+                        position: 'center',
+                        icon: 'success',
+                        title: 'Se ha agregado correctamente',
+                        showConfirmButton: false,
+                        timer: 1500
+                    }).then(function () {
+                        window.location.reload()
+                    });
+                }
             })
-                .done(function (data, res) {
-                    if(data.hasOwnProperty('error')){
-                        Swal.fire({
-                            position: 'center',
-                            icon: 'error',
-                            title: 'Ha ocurrido un error',
-                            showConfirmButton: false,
-                            timer: 1500
-                        })
-                    }else{
-                        Swal.fire({
-                            position: 'center',
-                            icon: 'success',
-                            title: 'Se ha agregado correctamente',
-                            showConfirmButton: false,
-                            timer: 1500
-                        }).then(function () {
-                            window.location.reload()
-                        });
-                    }
-                })
-        }
-    }else{
+            
+    } else {
 
     }
 }
